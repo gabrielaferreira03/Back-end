a4326862a400c3229381c8c02055a9d847c98db7
Gabriela da Silva Ferreira <gabriela.s.ferreira@edu.senai.br>
2026-09-17 16:37:06 -0300

    Delete ex4.php

--- ex4.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nome_cliente = "    Gabriela da Silva Ferreira";
-
-echo "Cliente: " . ltrim($nome_cliente);
-
-$nome_cliente2 = "Isadora Musk   ";
-echo "<br> Cliente: ".rtrim($nome_cliente2);
-?>
